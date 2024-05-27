0e460c779db4ff06d38c93e3ab723e3150af4284
RevanArturito <revan.arturito@gmail.com>
2024-05-27 21:42:59 +0700

    Inisialisasi Class Reservasi

--- tubespbo/src/Model/Reservasi.java
+++ tubespbo/src/Model/Reservasi.java
@@ -5,10 +5,74 @@
  */
 package Model;
 
+import java.sql.Date;
+
 /**
  *
  * @author hafid
  */
 public class Reservasi {
-    
+    private String nama_pasien;
+    private String nama_dokter;
+    private String nomor_telepon;
+    private Date tannggal_reservasi;
+    private Date waktu_reservasi;
+    private String status;
+
+    public Reservasi(String nama_pasien, String nama_dokter, String nomor_telepon, Date tannggal_reservasi, Date waktu_reservasi, String status) {
+        this.nama_pasien = nama_pasien;
+        this.nama_dokter = nama_dokter;
+        this.nomor_telepon = nomor_telepon;
+        this.tannggal_reservasi = tannggal_reservasi;
+        this.waktu_reservasi = waktu_reservasi;
+        this.status = status;
+    }
+
+    public String getNama_pasien() {
+        return nama_pasien;
+    }
+
+    public void setNama_pasien(String nama_pasien) {
+        this.nama_pasien = nama_pasien;
+    }
+
+    public String getNama_dokter() {
+        return nama_dokter;
+    }
+
+    public void setNama_dokter(String nama_dokter) {
+        this.nama_dokter = nama_dokter;
+    }
+
+    public String getNomor_telepon() {
+        return nomor_telepon;
+    }
+
+    public void setNomor_telepon(String nomor_telepon) {
+        this.nomor_telepon = nomor_telepon;
+    }
+
+    public Date getTannggal_reservasi() {
+        return tannggal_reservasi;
+    }
+
+    public void setTannggal_reservasi(Date tannggal_reservasi) {
+        this.tannggal_reservasi = tannggal_reservasi;
+    }
+
+    public Date getWaktu_reservasi() {
+        return waktu_reservasi;
+    }
+
+    public void setWaktu_reservasi(Date waktu_reservasi) {
+        this.waktu_reservasi = waktu_reservasi;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
